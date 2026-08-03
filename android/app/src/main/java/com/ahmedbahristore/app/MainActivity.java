1ed090f634ae29edcb711f90c2442f03c7dfcbc7
ahmediraqi727-oss <ahmediraqi727@gmail.com>
2026-08-03 23:51:04 +0300

    Add WebView debugging to MainActivity.java to fix Android splash screen stuck issue

--- android/app/src/main/java/com/ahmedbahristore/app/MainActivity.java
+++ android/app/src/main/java/com/ahmedbahristore/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.ahmedbahristore.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable WebView remote debugging via chrome://inspect
+        // This also helps prevent splash screen from staying stuck
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
